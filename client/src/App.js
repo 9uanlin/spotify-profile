636e5168be0630b1e41e18e43cdfe94fbfd02fb1
Roy Lee <rlee09@cs.students.ubc.ca>
2023-09-25 15:30:27 -0700

    Setup Styled Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
 import { accessToken, logout, getCurrentUserProfile } from './spotify';
 import { catchErrors } from './utils';
-import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
-import './App.css';
+import { GlobalStyle } from './styles';
+import styled from 'styled-components/macro';
+
+const StyledLoginButton = styled.a`
+  background-color: var(--green);
+  color: var(--white);
+  padding: 10px 20px;
+  margin: 20px;
+  border-radius: 30px;
+  display: inline-block;
+`;
 
 // Scroll to top of page when changing routes
 function ScrollToTop() {
@@ -32,11 +42,13 @@ function App() {
 
   return (
     <div className="App">
+      <GlobalStyle />
+
       <header className="App-header">
         {!token ? (
-          <a className="App-link" href="http://localhost:8888/login">
+          <StyledLoginButton href="http://localhost:8888/login">
             Log in to Spotify
-          </a>
+          </StyledLoginButton>
         ) : (
           <Router>
             <ScrollToTop />
